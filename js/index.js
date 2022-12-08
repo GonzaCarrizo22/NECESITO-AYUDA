a8f3cf99b48b3b635eec46dde90e77409ce1f8b7
DarwinSCGDev <darwinscgdev@gmail.com>
2022-12-08 20:53:24 +0100

    funcionalidad de busqueda
    Se ha completado la funcionalidad de buscar un personaje en funcion del nombre

--- js/index.js
+++ js/index.js
@@ -34,6 +34,62 @@ getCharacters(data => {
 
 });    
 
+// funcion para saber el contenido de un objeto o un string
+function isEmpty(node){
+    if(typeof node === 'object'){
+        return !node.hasChildNodes();
+    }else{
+        return node === "";
+    }
+}
+// funcion para vaciar el contenedor donde se muestran los personajes
+function removeCharactersOfContainer(){
+    const main = document.querySelector("main");
+    while(main.hasChildNodes()){
+        main.removeChild(main.firstChild);
+    }
+}
+// se hace sincrona debido a que la api no es sincrona
+async function getCharacterByName(){
+    const characterName = document.getElementById("formulario").value;
+    const main = document.querySelector("main");
+    const sleep = (delay) => new Promise((resolve) => setTimeout(resolve,delay)); // permite soncronizar
+    getCharacters(data => {
+        data.results.forEach(personaje => {
+            const name = personaje.name.toLowerCase();
+            if(name.includes(characterName.toLowerCase())){
+                const article = document.createRange().createContextualFragment(/*html*/`
+                    <article>
 
+                        <div class="image-container">
+                            <img src="${personaje.image}" alt="Personaje">
+                        </div>
+
+                        <h2>${personaje.name}</h2>
+                        <span>${personaje.status}</span>
+                        
+                    </article>
+
+                `);
+                main.append(article);
+            }
+        });
+    });
+    await sleep(100); // sincronizamos 
+    // control de no conincidencias
+    if(isEmpty(main)){
+        const h1 = document.createElement("h1");
+        h1.innerHTML = "No se encontraron personajes.";
+        main.append(h1);
+    }
+    
+}
+// evento del boton buscar
+document.getElementById("boton").addEventListener("click",function(){
+    if(!isEmpty(document.getElementById("formulario").value)){
+        removeCharactersOfContainer();
+        getCharacterByName();
+    }
+});
 
         
